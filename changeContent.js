b9f0e9db0805bd89cc41b46a1b0b8d031b73606f
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-08-17 19:45:45 -0700

    Fixed alot of things

--- changeContent.js
+++ changeContent.js
@@ -4,6 +4,7 @@ function clicked(name) {
     document.getElementById('hillclimb').style.display="none";
     document.getElementById('scouter').style.display="none";
     document.getElementById('evol').style.display="none";
+    document.getElementById('mirror').style.display="none";
     document.getElementById(name).style.display="block";
     return false;
   }
